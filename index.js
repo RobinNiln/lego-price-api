9a916a446b35b36ae83339af5e02423051bb9b78
RobinNiln <robin.nilsson@ntm.se>
2026-03-24 08:46:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const PORT = process.env.PORT || 3001;
 // CORS – allow bestlegoprice.com and localhost
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
